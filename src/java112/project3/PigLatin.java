be49560c7a3b116ab707fdcc9d0a7eae17ce3902
Sam Miller <sdmiller1@madisoncollege.edu>
2019-11-12 14:06:03 -0600

    Finished the logic but still unstyled

--- src/java112/project3/PigLatin.java
+++ src/java112/project3/PigLatin.java
@@ -5,28 +5,49 @@ import java.util.*;
 public class PigLatin {
 
     private String translation;
+    private List<String> vowels = new ArrayList<String>();
+
+    public PigLatin() {
+        vowels.add("a");
+        vowels.add("e");
+        vowels.add("i");
+        vowels.add("o");
+        vowels.add("u");
+    }
 
     public String getTranslation() {
         return translation;
     }
 
     public void setTranslation(String english) {
-        this.translation = convertToPigLatin(english);
+        this.translation = translate(english);
     }
 
-    public String convertToPigLatin(String english) {
+    public String translate(String english) {
 
         List<String> translatedWords = new ArrayList<String>();
 
         for (String word : english.split("\\W")) {
             if (word.length() > 0) {
-                //TODO make words piglatin
-                translatedWords.add(word);
+                translatedWords.add(convertToPigLatin(word.toLowerCase()));
             }
         }
 
         return String.join(" ", translatedWords);
-        // return english;
+    }
+
+    public String convertToPigLatin(String word) {
+
+        String pig = "";
+
+        if (!vowels.contains(word.substring(0, 1))) {
+            pig = word.substring(1) + word.substring(0, 1) + "ay";
+        } else {
+            pig = word + "yay";
+        }
+
+        return pig;
+
     }
 
 }
